391e75d874054d6e5b4d57df2967ce1a4d931496
Brian Nickila <brian.nickila@gmail.com>
2019-04-01 22:48:13 -0500

    Adjusted some colors.

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -18,6 +18,9 @@ function Navbar() {
                     <li class="nav-item">
                         <Link class="nav-link" to="/contact">CONTACT</Link>
                     </li>
+                    <li class="nav-item">
+                        <Link class="nav-link" to="/resume">RESUME</Link>
+                    </li>
                 </ul>
             </div>
         </nav>
--- client/src/pages/Resume.js
+++ client/src/pages/Resume.js
@@ -3,9 +3,9 @@ import "./Resume.css";
 
 function Resume() {
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md-12">
+        <div className="container-fluid">
+            <div className="row resume-div">
+                <div className="col-md-8 mx-auto">
                     <iframe src="https://docs.google.com/document/d/e/2PACX-1vTxdGRAMEyBJ4XSmizr0GYREMctHnwACPss04Zdkyh7l3jICyIwLMC02s-HGOe_kr3zsUiWzF8dT2jo/pub?embedded=true"></iframe>
                 </div>
             </div>
